e8ab2e8e7f803f93afd9d2c9e260157a2dcf7d56
Juan Chung <jcdev@Juans-MacBook-Pro.local>
2023-08-18 09:48:44 -0700

    second commit

---
+++ src/GamePanel.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+    GamePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+    public void gameOver(Graphics g) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            
+        }
+    }
+    
+}
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,6 +1,8 @@
-public class App {
+public class SnakeGame {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   
+        new GameFrame();
+    
     }
 }
 
