42a12ad9e006d49ab5c3665a5727d9e261c9a48c
Juan Chung <jcdev@Juans-MacBook-Pro.local>
2023-08-18 11:21:27 -0700

    Movement Allowed

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,7 +9,7 @@ public class GamePanel extends JPanel implements ActionListener{
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
-    static final int DELAY = 75;
+    static final int DELAY = 100;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int bodyParts = 6;
@@ -50,6 +50,17 @@ public class GamePanel extends JPanel implements ActionListener{
         }
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
 
     public void newApple() {
@@ -59,15 +70,57 @@ public class GamePanel extends JPanel implements ActionListener{
     }
 
     public void move() {
+        for(int i = bodyParts; i>0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        switch(direction) {
+        case 'U':
+            y[0] = y[0] - UNIT_SIZE;
+            break;
+        case 'D':
+            y[0] = y[0] + UNIT_SIZE;
+            break;
+        case 'R':
+            x[0] = x[0] + UNIT_SIZE;
+            break;
+        case 'L':
+            x[0] = x[0] - UNIT_SIZE;
+            break;
+        }
 
     }
 
     public void checkApple() {
+        if((x[0] == appleX) && y[0] == appleY) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
 
     }
 
     public void checkCollisions() {
-
+        for (int i = bodyParts; i>0; i--) {
+            if((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+        if (x[0] < 0) {
+            running = false;
+        }
+        if (x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        if (y[0] < 0) {
+            running = false;
+        }
+        if (y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+        if(!running) {
+            timer.stop();
+        }
     }
     public void gameOver(Graphics g) {
 
@@ -75,14 +128,41 @@ public class GamePanel extends JPanel implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+
+        if(running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
+
     }
 
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            
+            switch(e.getKeyCode()) {
+            case KeyEvent.VK_LEFT:
+                if(direction != 'R') {
+                    direction = 'L';
+                }
+                break;
+            case KeyEvent.VK_RIGHT:
+                if(direction != 'L') {
+                    direction = 'R';
+                }
+                break;
+            case KeyEvent.VK_UP:
+                if(direction != 'D') {
+                    direction = 'U';
+                }
+                break;
+            case KeyEvent.VK_DOWN:
+                if(direction != 'U') {
+                    direction = 'D';
+                }
+                break;
+            }
         }
     }
     
